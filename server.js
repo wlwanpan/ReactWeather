3f63802446a47c22aff6f0f9462942815fb5edee
wlwannpan <wlwanpan@uwaterloo.ca>
2017-03-26 14:12:37 -0700

    rename example

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 3000;
 
+// Fix https request to http if needed
+// weather website api only works on http
 app.use(function (req, res, next) {
     if (req.headers['x-forwarded-proto'] === 'https') {
         res.redirect('http://' + req.hostname + req.url);
