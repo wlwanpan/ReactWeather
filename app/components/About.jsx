0013ee0dd6b9f9336a757896ebb38fad4afb8264
wlwannpan <wlwanpan@uwaterloo.ca>
2017-03-26 23:29:08 -0700

    Updated about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -15,8 +15,20 @@ var React = require('react');
 var About = (props) => {
     return (
         <div>
-            <h3>About</h3>
-            <p>Welcome to the About page</p>
+            <h1 className="text-center">About</h1>
+            <p>This is a weather application project based on a React Web App course on Udemy</p>
+            <p></p>
+            <ul>
+                <li>
+                    Js framework used: <a href="https://facebook.github.io/react/" target="_blank">React</a>
+                </li>
+                <li>
+                    CSS framework used:<a href="http://foundation.zurb.com/" target="_blank">Foundation</a>
+                </li>
+                <li>
+                    API requested from:<a href="https://openweathermap.org/" target="_blank">OpenWeatherMap</a>
+                </li>
+            </ul>
         </div>
     )
 };
